4e4b09efabfa0f7190a063145a25576b83e3894c
LUCAS KELLER DA SILVA <lucas_keller-silva@estudante.sesisenai.org.br>
2023-09-30 09:15:40 -0300

    added back to top button

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/* let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+} */
+
+// Get a reference to the button element
+const button = document.getElementById('fixedButton');
+
+// Function to show the button when the user scrolls past a certain point
+function toggleButton() {
+  if (window.scrollY > 400) {
+    button.classList.remove('hidden');
+  } else {
+    button.classList.add('hidden');
+  }
+}
+
+// Add an event listener to the window's scroll event
+window.addEventListener('scroll', toggleButton);
+
+// Function to smoothly scroll to the top when the button is clicked
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Add a click event listener to the button
+button.addEventListener('click', scrollToTop);
